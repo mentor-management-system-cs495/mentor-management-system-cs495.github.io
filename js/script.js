e31ddb0510b3f5f52f05bcb83a375634d2e493c2
hrhazelwood <hrhazelwood@crimson.ua.edu>
2025-04-25 04:46:32 -0500

    start of faq section

--- js/script.js
+++ js/script.js
@@ -95,6 +95,24 @@ tabsContainerModify.addEventListener("click", (e) => {
         modifySection.querySelector(target).classList.add("active");
     }
 });
+
+/* -------------- FAQ Collapsibles ----------------------- */
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activeFAQ");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 /* -------------- Portfolio Item Popup ----------------------- */
 document.addEventListener("click", (e) => {
     if (e.target.classList.contains("view-project-btn")) {
